c9998844df069a2b2f000b263734eba9eddbb244
kursadaydin <kursad6129@gmail.com>
2022-03-30 00:28:02 +0300

    Düzeltmeler Devam Ediyor

--- my_manager.py
+++ my_manager.py
@@ -4,6 +4,9 @@ import urllib.parse, urllib.request, urllib.error
 import xml.etree.ElementTree as ET
 import json
 from requests import Session
+import pandas as pd
+
+from datetime import datetime
 
 
 series = None
@@ -23,14 +26,36 @@ class My_Manager:
         self.formulas = _formulas
         self.frequency = _frequency
         
+    def convertTime(self,timestamp):
+        time = datetime.fromtimestamp(int(timestamp)).strftime('%d-%m-%y')
+        return time
 
 
     def get_data(self):
+        temp={}
         url = a.MAIN_PART
         path ='{}series={}&startDate={}&endDate={}&type=json&key={}&aggregationTypes={}&Formulas={}&Frequency={}'.format(url,self.series,self.startDate,self.endDate,a.API_KEY,self.aggregationTypes,self.formulas,self.frequency)
         
+        #data = pd.read_json(path)
+        #df = pd.DataFrame(data)
+
+        
         url_to_open = urllib.request.urlopen(path).read()
         data = json.loads(url_to_open)
+        
+        for i in data['items']:
+            if i['TP_DK_EUR_A_YTL'] == None:
+                continue
+            tempTime = self.convertTime(i['UNIXTIME']['$numberLong'])
+            tempValue = i['TP_DK_EUR_A_YTL']
+            temp['Time']= tempTime
+            temp['Value'] = tempValue
+            #print('{} :  {}'.format( tempTime, tempValue))
+    
+        df = pd.DataFrame.from_dict(temp, orient='index')
+        print(df)
+        
+        
         #if url_to_open.ok:
         #    pass
         #else:
@@ -49,4 +74,4 @@ class My_Manager:
         #tree = ET.parse(url_to_open)
         #root_node = tree.getroot()
         #lst = root_node.findall('TP_DK_USD_A_YTL')  
-        return data
+        
