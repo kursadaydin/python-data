92a7affd87bbcd5697b619a0b06de5846f3f711b
kursadaydin <kursad6129@gmail.com>
2022-03-30 15:15:28 +0300

    Çalışmalar Devam Ediyor

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import abbr as a
 
 if __name__ == '__main__':
     
-    dolar_alis = a.series.USD_ALIS.value
+    dolar_alis = a.series.ORT_KONUT_FAIZ_ORANI.value
     startDate ='01-01-2020'
-    endDate ='01-01-2022'
+    endDate ='01-04-2022'
     formulas = a.formulas.DUZEY.value
-    frequency = a.frequency.AYLIK.value
+    frequency = a.frequency.YILLIK.value
     aggregation = a.aggregationTypes.BITIS.value
     
     deneme = My_Manager(_series=dolar_alis, start_Date=startDate,end_Date=endDate, aggregation_Types=aggregation,_formulas =formulas,_frequency = frequency)
-    print(deneme.get_data())
+    deneme.get_data()
     
     
 
--- my_manager.py
+++ my_manager.py
@@ -7,6 +7,7 @@ from requests import Session
 import pandas as pd
 
 from datetime import datetime
+import numpy as np
 
 
 series = None
@@ -36,23 +37,33 @@ class My_Manager:
         url = a.MAIN_PART
         path ='{}series={}&startDate={}&endDate={}&type=json&key={}&aggregationTypes={}&Formulas={}&Frequency={}'.format(url,self.series,self.startDate,self.endDate,a.API_KEY,self.aggregationTypes,self.formulas,self.frequency)
         
-        #data = pd.read_json(path)
-        #df = pd.DataFrame(data)
-
-        
+        #df_path = pd.read_json(path)
+        #df = pd.DataFrame(data=df_path, columns=['Tarih','TP_DK_EUR_A_YTL'])
+       
         url_to_open = urllib.request.urlopen(path).read()
         data = json.loads(url_to_open)
         
-        new_series = self.series.replace('.','_')
+        new_series = self.series.replace('.','_') #URL'deki seri adı ile JSON sonuçundaki seri adı farklı geliyor.
+        temp_list = [[], []]
         for i in data['items']:
             if i[new_series] == None:
                 continue
-            temp['Time'] = self.convertTime(i['UNIXTIME']['$numberLong'])
-            temp['Value'] = i[new_series]
-            print('{} :  {}'.format( temp['Time'], temp['Value']))
-    
-        #df = pd.DataFrame.from_dict(temp, orient='index')
-        #print(df)
+            #temp['Time'] = self.convertTime(i['UNIXTIME']['$numberLong'])
+            #temp['Time'] = (i['Tarih'])
+            #temp['Value'] = i[new_series]
+            temp[i['Tarih']] = i[new_series]
+            #temp_list[0].append(i['Tarih'])
+            #temp_list[1].append(i[new_series])
+            #my_np =np.array([[temp['Time']], [temp['Value']]])
+            #print('{} :  {}'.format( temp['Time'], temp['Value']))
+            #print('{} : {}'.format(temp_list[0][i],temp_list[1][i]))
+            
+        for x, y in temp.items():
+            print(x, y)
+  
+         
+        
+        
         
         
         #if url_to_open.ok:
